c3c02e4e6b1261f4845e26b3b5d8b718fe02e6bb
mahdignb <qanbarim327@hotmail.com>
2022-11-19 11:52:32 +0330

    Added book controller with a test API

---
+++ WebAPI/Controllers/BookController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        [HttpGet]
+        [Route("test")]
+        public string Test()
+        {
+            return "Hello world";
+        }
+    }
+}
