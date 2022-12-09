e940d033c533e3bb2cb5160b6e6b368b9ce11ebe
mahdignb <qanbarim327@hotmail.com>
2022-12-09 23:29:33 +0330

    Added BookishDbContext

--- Persistence/BookishDbContext.cs
+++ Persistence/BookishDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Domain.Entities;
+﻿using Domain.Entities.Account;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
