4c889dc742d5df801b1056aea85fa09a18fa89d5
mahdignb <qanbarim327@hotmail.com>
2022-12-09 23:24:24 +0330

    Added MediatR base controller

---
+++ WebAPI/Controllers/ApiControllerBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class ApiControllerBase : ControllerBase
+    {
+        private ISender _mediator = null!;
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
