bd9c66324257aec7f9f2e51f9e27f14f74ab658c
Mahdi Ghanbari <qanbarim326@gmail.com>
2022-12-30 21:59:09 +0330

    Added bookishDbContext interface and references

---
+++ Core/Common/Interfaces/IBookishDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities.Bookish;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Common.Interfaces
+{
+    public interface IBookishDbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+    }
+}
