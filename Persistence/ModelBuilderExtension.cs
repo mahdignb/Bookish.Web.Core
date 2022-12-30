e8e5c704fb5e51de5e138de3c1759a3215dfa9d9
Mahdi Ghanbari <qanbarim326@gmail.com>
2022-12-30 22:02:01 +0330

    Added model builder extension for seeding data

---
+++ Persistence/ModelBuilderExtension.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entities.Account;
+using Domain.Entities.Bookish;
+using Domain.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder builder)
+        {
+            //Roles
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "User",
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+                NormalizedName = "USER"
+            });
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Admin",
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+                NormalizedName = "ADMIN"
+            });
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "SuperAdmin",
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+                NormalizedName = "SUPERADMIN"
+            });
+        }
+    }
+}
