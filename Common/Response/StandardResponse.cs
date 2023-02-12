bb2e482916672ed973f7e6746ac40d5514c5564b
mahdignb <qanbarim327@hotmail.com>
2023-02-13 01:04:44 +0330

    Added standard response class

---
+++ Common/Response/StandardResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Response
+{
+    public class StandardResponse<T>
+    {
+        public string ResponseText { get; set; }
+        public string ResponseStatus { get; set; }
+        public T Data { get; set; }
+    }
+
+    public enum ResponseStatus
+    {
+        Success,
+        Failed
+    }
+}
